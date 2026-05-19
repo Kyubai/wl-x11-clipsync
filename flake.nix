851c4e443816fb83a4388053cb748b0e1acd65f8
Matthias Riegel <public@verriegelt.net>
2026-05-19 17:44:27 +0200

    added flake build

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      name = "clipsync";
+      script = builtins.readFile ./clipsync.py;
+      program =
+        pkgs.writers.writePython3Bin name
+        {
+          doCheck = false;
+          makeWrapperArgs = [
+            "--prefix"
+            "PATH"
+            ":"
+            "${pkgs.lib.makeBinPath [
+              pkgs.xclip
+              pkgs.clipnotify
+            ]}"
+          ];
+        }
+        script;
+    in {
+      packages = {
+        "${name}" = program;
+        default = self.packages.${system}.${name};
+      };
+    });
+}
